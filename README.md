948f08cc750406b42551a9d87ffae45b86205262
Spencer Kimball <spencercowleykimball@gmail.com>
2026-03-20 09:42:51 -0600

    Finished! Looks decent enough.

--- README.md
+++ README.md
@@ -7,11 +7,20 @@ Getting the initial input to output to the console log was a lot harder than I w
 
 I also was trying to console.log out specific variables at the end of the script.js, but not having it inside of a function, and it took me a minute to realize that it was outputting what the variable was at the beginning of the document which was nothing.
 
+I was really just trying to get the values and make sure I knew where they were and where they were going. And then after that I worried about the display and what values were showing up and when. There were some initial inconsistencies about the display showing some decimals and then not having the 0 at the beginning of the decimal when different radio buttons were selected, so just trying to make everything be consistent as the user selected the different options.
+
+Then it was fun choosing the colors and making it look pretty with the CSS at the end.
+
 **What was the most difficult problem of this assignment and how did you work through it?**
 
-
+I think it was hard to understand that I could change the inner html without having to return something inside of a function. I feel like the other functions that I have worked with generally don't affect the global variables unless they return a value that is then saved at the function call. It was a game changer once w3 schools taught me how to save a variable that is the id of an element in the html, and then change its innerHTML property.
 
 **What features would you like to add to this tip calculator in the future to make it more robust?**
 
+I would not want to show the final price until after the tip percentage radio button has been selected. Currently if someone types in a value for the initial price, then it shows that value as the final price, which isn't true.
 
+Hilarious to realize that what I had just said was actually the first bullet point on the assignment and I didn't read it until just now. Glad I was able to add a necessary feature to this assignment to make it "more robust".
 
+I currently have the width as fixed values, so if the screen was really small then the calculator would be going off the side of the page. So if I wanted to make it more robust then I could throw in some media queries to decrease the size of the calculator for an iPhone SE. Currently on the iPhone SE screen it is off centered which doesn't look super professional.
+
+For maintenance purposes, my variable names are currently pretty bad. Updating those to make more sense to the next developer would probably be pretty useful. I tried to throw in as many comments as I could to the js file to try and combat that.
--- script.js
+++ script.js
@@ -1,28 +1,53 @@
-// FIXME!! Currently I have the tip and the initial cost values, but now
-// I need to inject them into a p tag in the html
-// and make the caculator presentable!
-
 
 // Setting the variable initial_cost to the starting input of the DOM text box, which is currently nothing
 // and this value won't change until the addEventListener function happens.
 // let initial_cost = document.querySelector("#numInput").value;
 let initial_cost = 0;
-let final_value = 0;
+let final_value = document.getElementById("final_price");
+
+// This is the html p object in the html
+let current_tip_p = document.getElementById("selected_tip");
+
+// The initial cost plus the tip total
+let total = 0;
+
+// Set an initial default value for the tip value based on the radio button
+// that was chosen to be the default
+let tip_value = .1;
+
+// This value will be the initial cost multiplied by the tip percentage
+let tip_total = 0;
+
+// To have one of the radio buttons selected at the beginning
+let initial_radio = document.getElementById('tip1');
+initial_radio.checked = true;
 
 // Outputting to the console log the current value of the "Initial Price"
 // This function is triggered when the user starts typing inside of the text box
 numInput.addEventListener("input", function () {
     initial_cost = Number(numInput.value);
-    console.log(numInput.value)
+    calculateTotal();
 })
 
 
 function calculateTotal() {
-    console.log("Final Tip Value: ", tip_value*initial_cost + initial_cost);
+    // The global variables have all been updated at this point
+    // just need to calculate the totals
+    tip_total = tip_value*initial_cost;
+    total = tip_total + initial_cost;
+
+    // Round to two decimal places
+    total = total.toFixed(2);
+
+    // Change final_value's innerHTML attribute to be the 
+    // final string of Final Price with the total value
+    final_value.innerHTML = "Final Price: " + total;
+    current_tip_p.innerHTML = "Selected Tip Amount: " + tip_total.toFixed(2);
 }
 
-let tip_value;
 
+// Set the global variable of tip value to the value inside of 
+//  the tip1 input radio button
 tip1.addEventListener("input", function () {
     tip_value = tip1.value;
     calculateTotal();
@@ -41,13 +66,10 @@ tip4.addEventListener("input", function () {
 })
 
 
-// These don't output any values because at the beginning of the script they aren't given any values!
-// console.log(tip_value);
-// console.log(tip_value*numInput.value + numInput.value);
 
-// blah blah is outputted to the web console because it is given a value directly in the script!
-// let new_val = "blah blah";
-// console.log(new_val);
+
+
+
 
 
 
@@ -66,9 +88,15 @@ tip4.addEventListener("input", function () {
 //             console.log(radio.value);
 //         }
 //     });
-
-
-
+// 
+// 
+// These don't output any values because at the beginning of the script they aren't given any values!
+// console.log(tip_value);
+// console.log(tip_value*numInput.value + numInput.value);
+// 
+// blah blah is outputted to the web console because it is given a value directly in the script!
+// let new_val = "blah blah";
+// console.log(new_val);
 
 
 
